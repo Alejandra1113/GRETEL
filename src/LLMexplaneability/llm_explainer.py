be20f1d45de6052bdaa608129cc0a93921cfe2a7
Alejandra <amonzon1113@gmail.com>
2025-11-12 11:35:56 +0100

    Refactor LLMExplainer to integrate OllamaExplainer
    - Added OllamaExplainer class that extends LLMExplainer, implementing the explain_counterfactual method using the Ollama client.
    - Introduced export_explanation method in LLMExplainer for saving explanations to a file.
    - Updated references in evaluation stages to use OllamaExplainer instead of GeminiExplainer for generating explanations.

--- src/LLMexplaneability/llm_explainer.py
+++ src/LLMexplaneability/llm_explainer.py
@@ -1,4 +1,5 @@
 from google import genai
+import ollama
 import os
 import time
 import random as rd
@@ -12,6 +13,15 @@ class LLMExplainer:
     def explain_counterfactual(self, prompt):
         pass
 
+    def export_explanation(self, explanation = None, prompt = None, file = "explanation.txt"):
+        if explanation == None:
+            explanation = self.explain_counterfactual(prompt)
+        
+        with open(file, "w", encoding="utf-8") as f:
+            f.write(explanation)          # sobrescribe si existe
+    
+
+
 
 class GeminiExplainer(LLMExplainer):
 
@@ -21,9 +31,6 @@ class GeminiExplainer(LLMExplainer):
         super().__init__(api_key, model)
 
     def explain_counterfactual(self, prompt):
-
-        seconds = rd.randint(1, 20)
-        time.sleep(seconds) # wait for 60 seconds before retrying
         client = genai.Client(api_key = self.api_key)
 
         num_tries = 0
@@ -43,10 +50,33 @@ class GeminiExplainer(LLMExplainer):
   
 
     
-    def export_explanation(self, explanation = None, prompt = None, file = "explanation.txt"):
-        if explanation == None:
-            explanation = self.explain_counterfactual(prompt)
-        
-        with open(file, "w", encoding="utf-8") as f:
-            f.write(explanation)          # sobrescribe si existe
-    
+
+class OllamaExplainer(LLMExplainer):
+
+    def __init__(self):
+        api_key= None
+        model = "llama3"
+        super().__init__(api_key, model)
+
+    def explain_counterfactual(self, prompt):
+
+        seconds = rd.randint(1, 20)
+        # time.sleep(seconds) # wait for 60 seconds before retrying
+        client = ollama.Client()
+
+
+        num_tries = 0
+        # while num_tries < 30:
+        try:
+            resp = client.generate(
+                model=self.model,
+                prompt=prompt,
+                stream=False # Configuramos stream en False para obtener la respuesta completa de una vez
+            )
+            
+            return resp.response
+        except Exception as e:
+            num_tries += 1
+            # time.sleep(60) # wait for 60 seconds before retrying
+            
+        # raise Exception("Failed to get response from the model after multiple attempts.")
